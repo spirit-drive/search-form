dae87b8f1ebc971744d39e3a88dcab44c1343ffc
Igor Zvyagin <spirit-drive@yandex.ru>
2018-10-31 15:05:33 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchForm from "./components/SearchForm/SearchForm";
 
 class App extends Component {
     render () {
         return (
-            <div>!</div>
+            <div>
+                <SearchForm/>
+            </div>
         )
     }
 }
