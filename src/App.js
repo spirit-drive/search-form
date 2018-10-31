648243718e31da9e205fef2710d685e2e2240ff2
Igor Zvyagin <spirit-drive@yandex.ru>
2018-10-31 15:30:27 +0300

    Первоначальная архитектура. Идем сверху вниз

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import SearchForm from "./components/SearchForm/SearchForm";
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: "",
+        };
+
+        this.setData = this.setData.bind(this);
+    }
+
+    _toPrepareData (data) {
+        return JSON.stringify(data, null, "  ");
+    }
+
+    setData (data) {
+        data = this._toPrepareData(data);
+        console.log(data);
+        this.setState({data});
+    }
+
     render () {
         return (
             <div>
-                <SearchForm/>
+                <SearchForm showResult={this.setData}/>
+                <div>{this.state.data}</div>
             </div>
         )
     }
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import db from "../../db";
 
 class SearchForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: db,
+        };
+
+    }
+
+    liftResult (data) {
+        return e => {
+            e.preventDefault();
+            this.props.showResult(data);
+        }
+    }
+
     render () {
         return (
-            <form>!</form>
+            <form onSubmit={this.liftResult(this.state.data)}>
+                <input type="submit" value="Найти"/>
+            </form>
         )
     }
 }
